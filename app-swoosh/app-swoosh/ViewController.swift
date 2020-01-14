a8cb5657fed6c9dede55f426f7a808c047caf956
omrobbie <putu.roby@yahoo.com>
2020-01-14 17:39:51 +0800

    auto layout with storyboard

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -15,8 +15,5 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        swoosh.frame = CGRect(x: view.frame.size.width / 2 - swoosh.frame.size.width / 2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
-        bgImg.frame = view.frame
     }
 }
