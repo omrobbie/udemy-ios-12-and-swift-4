082a386733aade4648112d33b398d769a44898f9
omrobbie <putu.roby@yahoo.com>
2020-01-15 11:38:32 +0800

    refactoring code, arrange with groups

---
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -0,0 +1,16 @@
+//
+//  LeagueVC.swift
+//  app-swoosh
+//
+//  Created by omrobbie on 15/01/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class LeagueVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ app-swoosh/app-swoosh/Controller/SkillVC.swift
@@ -0,0 +1,16 @@
+//
+//  SkillVC.swift
+//  app-swoosh
+//
+//  Created by omrobbie on 15/01/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class SkillVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
