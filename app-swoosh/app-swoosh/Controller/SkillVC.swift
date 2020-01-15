91f08d9907abdfaaa46931b4a77d504f0286afc1
omrobbie <putu.roby@yahoo.com>
2020-01-15 14:35:39 +0800

    finish the app

--- app-swoosh/app-swoosh/Controller/SkillVC.swift
+++ app-swoosh/app-swoosh/Controller/SkillVC.swift
@@ -12,9 +12,29 @@ class SkillVC: UIViewController {
 
     var player: Player!
 
+    @IBOutlet weak var finishBtn: BorderButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
 
-        print(player.desiredLeague!)
+    func selectSkill(skillLevel: String) {
+        player.selectedSkillLevel = skillLevel
+        finishBtn.isEnabled = true
+    }
+
+    @IBAction func onBeginnerTapped(_ sender: Any) {
+        selectSkill(skillLevel: "beginner")
+    }
+
+    @IBAction func onBallerTapped(_ sender: Any) {
+        selectSkill(skillLevel: "baller")
+    }
+
+    @IBAction func onFinishTapped(_ sender: Any) {
+        let alert = UIAlertController(title: "Player Information", message: "You are playing in \(player.desiredLeague!) league and you have \(player.selectedSkillLevel!) level", preferredStyle: .alert)
+
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
     }
 }
