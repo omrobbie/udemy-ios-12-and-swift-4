d7cd3ab91559a5778c32d50fd5cac71f76d2ff25
omrobbie <putu.roby@yahoo.com>
2020-01-15 10:53:18 +0800

    segue and unwind

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -10,10 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var swoosh: UIImageView!
-    @IBOutlet weak var bgImg: UIImageView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func unwindToFirstVC(_ unwindSegue: UIStoryboardSegue) {
     }
 }
