9afd57e6001db16fc20ba26b51a0aa12044a745c
omrobbie <putu.roby@yahoo.com>
2020-01-15 13:16:22 +0800

    refactor: use segue programmatically

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -13,4 +13,8 @@ class LeagueVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    @IBAction func onNextTapped(_ sender: Any) {
+        performSegue(withIdentifier: "skillVCSegue", sender: self)
+    }
 }
