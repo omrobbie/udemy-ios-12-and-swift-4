94b5367152f661490b70648174f87c9862b9163e
omrobbie <putu.roby@yahoo.com>
2020-01-16 12:03:20 +0800

    custom input accessory view for button

--- window-shopper/window-shopper/Controller/MainVC.swift
+++ window-shopper/window-shopper/Controller/MainVC.swift
@@ -10,7 +10,23 @@ import UIKit
 
 class MainVC: UIViewController {
 
+    @IBOutlet weak var wageTxt: CurrencyTextField!
+    @IBOutlet weak var priceTxt: CurrencyTextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 64))
+        calcBtn.backgroundColor = #colorLiteral(red: 0.8898937106, green: 0.5137273073, blue: 0.009626680054, alpha: 1)
+        calcBtn.setTitle("Calculate", for: .normal)
+        calcBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calcBtn.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+
+        wageTxt.inputAccessoryView = calcBtn
+        priceTxt.inputAccessoryView = calcBtn
+    }
+
+    @objc func calculate() {
+
     }
 }
