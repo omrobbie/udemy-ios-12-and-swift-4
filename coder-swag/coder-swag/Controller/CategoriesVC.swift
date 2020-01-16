6d4f38e2dbf3025b4c91a102a237fb58e82c7268
omrobbie <putu.roby@yahoo.com>
2020-01-16 15:29:53 +0800

    refactor folder structure, create category cell

--- coder-swag/coder-swag/Controller/CategoriesVC.swift
+++ coder-swag/coder-swag/Controller/CategoriesVC.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class CategoriesVC: UIViewController {
 
+    @IBOutlet weak var categoryTable: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
