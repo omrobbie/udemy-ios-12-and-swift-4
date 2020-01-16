32c983a8bf4936727ade5e4efe598bbf59cf4a2c
omrobbie <putu.roby@yahoo.com>
2020-01-16 11:55:40 +0800

    show custom text field to storyboard design

--- window-shopper/window-shopper/View/CurrencyTextField.swift
+++ window-shopper/window-shopper/View/CurrencyTextField.swift
@@ -8,11 +8,19 @@
 
 import UIKit
 
+@IBDesignable
 class CurrencyTextField: UITextField {
+
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
 
     override func awakeFromNib() {
         super.awakeFromNib()
+        customizeView()
+    }
 
+    func customizeView() {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
         layer.cornerRadius = 5.0
 
