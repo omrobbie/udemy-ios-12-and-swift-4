41dacd494db3665a96d9aa7abf6e65a231776aad
omrobbie <putu.roby@yahoo.com>
2020-01-16 20:09:45 +0800

    bugfix: collection view cell fix size for image width

--- coder-swag/coder-swag/Service/DataService.swift
+++ coder-swag/coder-swag/Service/DataService.swift
@@ -13,9 +13,9 @@ class DataService {
     static let instance = DataService()
 
     private let categories = [
-        Category(title: "SHIRTS", imageName: "shirts.png"),
-        Category(title: "HOODIES", imageName: "hoodies.png"),
         Category(title: "HATS", imageName: "hats.png"),
+        Category(title: "HOODIES", imageName: "hoodies.png"),
+        Category(title: "SHIRTS", imageName: "shirts.png"),
         Category(title: "DIGITAL", imageName: "digital.png")
     ]
 
