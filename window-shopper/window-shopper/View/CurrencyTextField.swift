79973700662dae87e83f9686f6ef41d98f2dd9dc
omrobbie <putu.roby@yahoo.com>
2020-01-16 14:35:57 +0800

    custom drawing using draw rect

--- window-shopper/window-shopper/View/CurrencyTextField.swift
+++ window-shopper/window-shopper/View/CurrencyTextField.swift
@@ -20,9 +20,28 @@ class CurrencyTextField: UITextField {
         customizeView()
     }
 
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLbl = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - size / 2, width: size, height: size))
+
+        currencyLbl.backgroundColor = #colorLiteral(red: 0.8782122731, green: 0.8783598542, blue: 0.878192842, alpha: 0.8017440581)
+        currencyLbl.textAlignment = .center
+        currencyLbl.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        currencyLbl.layer.cornerRadius = 5.0
+        currencyLbl.clipsToBounds = true
+
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+
+        currencyLbl.text = formatter.currencySymbol
+        addSubview(currencyLbl)
+    }
+
     func customizeView() {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
         layer.cornerRadius = 5.0
+        clipsToBounds = true
 
         textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         textAlignment = .center
