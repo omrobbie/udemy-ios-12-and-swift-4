9b941c86a8e3d8516a89747628b88909a7fe9b9b
omrobbie <putu.roby@yahoo.com>
2020-01-16 12:21:52 +0800

    playing with unit testing

---
+++ window-shopper/window-shopperTests/window_shopperTests.swift
@@ -0,0 +1,37 @@
+//
+//  window_shopperTests.swift
+//  window-shopperTests
+//
+//  Created by omrobbie on 16/01/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import XCTest
+
+class window_shopperTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+    func testGetHours() {
+        XCTAssert(Wage.getHours(forWage: 25, andPrice: 100) == 4)
+        XCTAssert(Wage.getHours(forWage: 15.50, andPrice: 250.53) == 17)
+    }
+}
