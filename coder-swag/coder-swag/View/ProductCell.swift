d7fc44b87e801b63a1038043c59ac8bfbf1358ca
omrobbie <putu.roby@yahoo.com>
2020-01-16 19:00:25 +0800

    create class for product collection  view cell

---
+++ coder-swag/coder-swag/View/ProductCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by omrobbie on 16/01/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
