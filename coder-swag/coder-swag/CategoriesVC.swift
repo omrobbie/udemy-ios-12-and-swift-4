409b98d5b3f05f4d6f72982bf4685e170baada94
omrobbie <putu.roby@yahoo.com>
2020-01-16 14:46:35 +0800

    import assets

--- coder-swag/coder-swag/CategoriesVC.swift
+++ coder-swag/coder-swag/CategoriesVC.swift
@@ -8,13 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CategoriesVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
