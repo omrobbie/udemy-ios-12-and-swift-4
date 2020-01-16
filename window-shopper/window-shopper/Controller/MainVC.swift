9164c52e1dbc5ce0f36823c8d082381b04c999b9
omrobbie <putu.roby@yahoo.com>
2020-01-16 14:25:36 +0800

    writing logic for calculation

--- window-shopper/window-shopper/Controller/MainVC.swift
+++ window-shopper/window-shopper/Controller/MainVC.swift
@@ -12,6 +12,8 @@ class MainVC: UIViewController {
 
     @IBOutlet weak var wageTxt: CurrencyTextField!
     @IBOutlet weak var priceTxt: CurrencyTextField!
+    @IBOutlet weak var resultLbl: UILabel!
+    @IBOutlet weak var hoursLbl: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,9 +26,32 @@ class MainVC: UIViewController {
 
         wageTxt.inputAccessoryView = calcBtn
         priceTxt.inputAccessoryView = calcBtn
+
+        resultLbl.isHidden = true
+        hoursLbl.isHidden = true
     }
 
     @objc func calculate() {
+        if let wageTxt = wageTxt.text,
+            let priceTxt = priceTxt.text {
+            if let wage = Double(wageTxt),
+                let price = Double(priceTxt) {
+                view.endEditing(true)
 
+                resultLbl.isHidden = false
+                hoursLbl.isHidden = false
+
+                resultLbl.text = "\(Wage.getHours(forWage: wage, andPrice: price))"
+            }
+        }
+
+    }
+
+    @IBAction func clearCalculatorTapped(_ sender: Any) {
+        wageTxt.text = ""
+        priceTxt.text = ""
+
+        resultLbl.isHidden = true
+        hoursLbl.isHidden = true
     }
 }
