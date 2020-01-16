e108bb3e167583ff6b086a342ccc992a0e7ea039
omrobbie <putu.roby@yahoo.com>
2020-01-16 15:39:57 +0800

    create category model with private set parameter

---
+++ coder-swag/coder-swag/Model/Category.swift
@@ -0,0 +1,20 @@
+//
+//  Category.swift
+//  coder-swag
+//
+//  Created by omrobbie on 16/01/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import Foundation
+
+struct Category {
+
+    private(set) var title: String
+    private(set) var imageName: String
+
+    init(title: String, imageName: String) {
+        self.title = title
+        self.imageName = imageName
+    }
+}
