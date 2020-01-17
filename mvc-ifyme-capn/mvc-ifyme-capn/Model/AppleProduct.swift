5c373b432f57ad9add2e88e909959e5e2237ba0f
omrobbie <putu.roby@yahoo.com>
2020-01-17 16:24:38 +0800

    make properties read-only with private set

--- mvc-ifyme-capn/mvc-ifyme-capn/Model/AppleProduct.swift
+++ mvc-ifyme-capn/mvc-ifyme-capn/Model/AppleProduct.swift
@@ -10,9 +10,9 @@ import Foundation
 
 class AppleProduct {
 
-    var name: String
-    var color: String
-    var price: Double
+    private(set) var name: String
+    private(set) var color: String
+    private(set) var price: Double
 
     init(name: String, color: String, price: Double) {
         self.name = name
