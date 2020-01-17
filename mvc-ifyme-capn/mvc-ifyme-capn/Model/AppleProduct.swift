7a8663ce58bd01dc013816a219da716236aa33e4
omrobbie <putu.roby@yahoo.com>
2020-01-17 10:16:23 +0800

    create data model for apple products

---
+++ mvc-ifyme-capn/mvc-ifyme-capn/Model/AppleProduct.swift
@@ -0,0 +1,22 @@
+//
+//  AppleProduct.swift
+//  mvc-ifyme-capn
+//
+//  Created by omrobbie on 17/01/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import Foundation
+
+class AppleProduct {
+
+    var name: String
+    var color: String
+    var price: Double
+
+    init(name: String, color: String, price: Double) {
+        self.name = name
+        self.color = color
+        self.price = price
+    }
+}
