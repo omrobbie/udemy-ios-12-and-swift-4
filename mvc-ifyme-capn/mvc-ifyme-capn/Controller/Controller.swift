bcdc6657155e83b36d0d4e56e518d2a944b37894
omrobbie <putu.roby@yahoo.com>
2020-01-17 10:12:58 +0800

    create mvc groups

--- mvc-ifyme-capn/mvc-ifyme-capn/Controller/Controller.swift
+++ mvc-ifyme-capn/mvc-ifyme-capn/Controller/Controller.swift
@@ -8,13 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class Controller: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
