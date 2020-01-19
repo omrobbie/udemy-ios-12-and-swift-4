a7186623a0a6fa5a673580954e756afc59f64556
omrobbie <putu.roby@yahoo.com>
2020-01-20 02:00:29 +0800

    move image with parallax

--- accelerometron/accelerometron/View/ParallaxCell.swift
+++ accelerometron/accelerometron/View/ParallaxCell.swift
@@ -13,8 +13,31 @@ class ParallaxCell: UITableViewCell {
     @IBOutlet weak var itemImageView: UIImageView!
     @IBOutlet weak var descriptionLabel: UILabel!
 
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupParallax()
+    }
+
     func configureCell(withImage image: UIImage, andDescription desc: String) {
         itemImageView.image = image
         descriptionLabel.text = desc
+    }
+
+    func setupParallax() {
+        let min = CGFloat(-30)
+        let max = CGFloat(30)
+
+        let xMotion = UIInterpolatingMotionEffect(keyPath: "layer.transform.translation.x", type: .tiltAlongHorizontalAxis)
+        xMotion.minimumRelativeValue = min
+        xMotion.maximumRelativeValue = max
+
+        let yMotion = UIInterpolatingMotionEffect(keyPath: "layer.transform.translation.y", type: .tiltAlongVerticalAxis)
+        yMotion.minimumRelativeValue = min
+        yMotion.maximumRelativeValue = max
+
+        let motionEffectGroup = UIMotionEffectGroup()
+        motionEffectGroup.motionEffects = [xMotion, yMotion]
+
+        itemImageView.addMotionEffect(motionEffectGroup)
     }
 }
