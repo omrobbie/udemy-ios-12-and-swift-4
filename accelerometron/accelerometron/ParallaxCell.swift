f0236b4e223184cb4bc2f3ffeeda34d01c2554a4
omrobbie <putu.roby@yahoo.com>
2020-01-20 00:37:36 +0800

    connect to table view cell

---
+++ accelerometron/accelerometron/ParallaxCell.swift
@@ -0,0 +1,15 @@
+//
+//  ParallaxCell.swift
+//  accelerometron
+//
+//  Created by omrobbie on 20/01/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class ParallaxCell: UITableViewCell {
+
+    @IBOutlet weak var itemImageView: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+}
