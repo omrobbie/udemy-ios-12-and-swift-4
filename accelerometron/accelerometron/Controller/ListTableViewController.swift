8e085f502c8dde75f9dbb02eff7f706f0c2c454c
omrobbie <putu.roby@yahoo.com>
2020-01-20 01:05:02 +0800

    show dummy data to table view controller

--- accelerometron/accelerometron/Controller/ListTableViewController.swift
+++ accelerometron/accelerometron/Controller/ListTableViewController.swift
@@ -12,5 +12,23 @@ class ListTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return imageArray.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "parallaxCell", for: indexPath) as? ParallaxCell else {return UITableViewCell()}
+        cell.configureCell(withImage: imageArray[indexPath.row], andDescription: nameArray[indexPath.row])
+
+        return cell
     }
 }
