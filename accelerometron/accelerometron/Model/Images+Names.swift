e16f4c9b0ce95aeae1cc010c473b1595ceb4bd0a
omrobbie <putu.roby@yahoo.com>
2020-01-20 00:52:56 +0800

    create dummy data

---
+++ accelerometron/accelerometron/Model/Images+Names.swift
@@ -0,0 +1,19 @@
+//
+//  Images+Names.swift
+//  accelerometron
+//
+//  Created by omrobbie on 20/01/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+var camera = UIImage(named: "1")!
+var city = UIImage(named: "2")!
+var animals = UIImage(named: "3")!
+var flowers = UIImage(named: "4")!
+var stand = UIImage(named: "5")!
+var urban = UIImage(named: "6")!
+
+var imageArray = [camera, city, animals, flowers, stand, urban]
+var nameArray = ["CAMERAS", "CITY", "ANIMALS", "FLOWERS", "STAND", "URBAN"]
