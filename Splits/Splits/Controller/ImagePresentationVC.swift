e3ff177abb382bcc99eb332514212243be047808
omrobbie <putu.roby@yahoo.com>
2020-01-20 05:43:24 +0800

    rename detail view controller to image presentation

--- Splits/Splits/Controller/ImagePresentationVC.swift
+++ Splits/Splits/Controller/ImagePresentationVC.swift
@@ -1,5 +1,5 @@
 //
-//  DetailViewController.swift
+//  ImagePresentationVC.swift
 //  Splits
 //
 //  Created by omrobbie on 20/01/20.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
+class ImagePresentationVC: UIViewController {
 
     @IBOutlet weak var ItemImageView: UIImageView!
 
