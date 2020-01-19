35d4e15f24a656e7b60a5bbb1e26ebcc5d98fd10
omrobbie <putu.roby@yahoo.com>
2020-01-20 05:38:10 +0800

    setup layout  for detail view controller

--- Splits/Splits/Controller/DetailViewController.swift
+++ Splits/Splits/Controller/DetailViewController.swift
@@ -10,31 +10,12 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
+    @IBOutlet weak var ItemImageView: UIImageView!
 
-
-    func configureView() {
-        // Update the user interface for the detail item.
-        if let detail = detailItem {
-            if let label = detailDescriptionLabel {
-                label.text = detail.description
-            }
-        }
-    }
+    var image: UIImage?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        configureView()
+        ItemImageView.image = image
     }
-
-    var detailItem: NSDate? {
-        didSet {
-            // Update the view.
-            configureView()
-        }
-    }
-
-
 }
-
