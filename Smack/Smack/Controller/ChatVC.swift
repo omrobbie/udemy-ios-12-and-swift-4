f2f72904c03367a6db4d7248a36165cd2f6e8696
omrobbie <putu.roby@yahoo.com>
2020-01-20 18:08:28 +0800

    bugfix: put segue set not push

--- Smack/Smack/Controller/ChatVC.swift
+++ Smack/Smack/Controller/ChatVC.swift
@@ -16,5 +16,8 @@ class ChatVC: UIViewController {
         super.viewDidLoad()
 
         menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+
+//        self.view.addGestureRecognizer(self.revealViewController).panGestureRecognizer())
+//        self.view.addGestureRecognizer(self.revealViewController).tapGestureRecognizer())
     }
 }
