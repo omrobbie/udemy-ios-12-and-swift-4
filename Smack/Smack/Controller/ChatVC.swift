f89b0855cd93143f0a0019622a36d69dec1af007
omrobbie <putu.roby@yahoo.com>
2020-01-20 17:35:29 +0800

    setup chat layout

--- Smack/Smack/Controller/ChatVC.swift
+++ Smack/Smack/Controller/ChatVC.swift
@@ -10,7 +10,11 @@ import UIKit
 
 class ChatVC: UIViewController {
 
+    @IBOutlet weak var menuBtn: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
     }
 }
