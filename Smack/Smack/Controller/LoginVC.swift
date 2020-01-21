b3203bff35bcc4c30594a3b3960746a191533ee3
omrobbie <putu.roby@yahoo.com>
2020-01-21 14:36:35 +0800

    create login view controller

---
+++ Smack/Smack/Controller/LoginVC.swift
@@ -0,0 +1,20 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by omrobbie on 21/01/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func closeBtnTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
