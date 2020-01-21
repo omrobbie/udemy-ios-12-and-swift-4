9c5bd2d669fcd171968d5c89c75c0e0bbcba81c4
omrobbie <putu.roby@yahoo.com>
2020-01-21 13:22:17 +0800

    create gradient view in inspectable

---
+++ Smack/Smack/View/GradientView.swift
@@ -0,0 +1,35 @@
+//
+//  GradientView.swift
+//  Smack
+//
+//  Created by omrobbie on 21/01/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class GradientView: UIView {
+
+    @IBInspectable var topColor: UIColor = #colorLiteral(red: 0.2901960784, green: 0.3019607843, blue: 0.8470588235, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+
+    @IBInspectable var bottomColor: UIColor = #colorLiteral(red: 0.1725490196, green: 0.831372549, blue: 0.8470588235, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+
+    override func layoutSubviews() {
+        let gradientLayer = CAGradientLayer()
+
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 0)
+    }
+}
