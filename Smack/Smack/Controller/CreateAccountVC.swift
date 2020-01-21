011b7bc9ebb4445040365d482a0b7cdb927aea3a
omrobbie <putu.roby@yahoo.com>
2020-01-21 14:52:18 +0800

    preparing create account view controller

---
+++ Smack/Smack/Controller/CreateAccountVC.swift
@@ -0,0 +1,16 @@
+//
+//  CreateAccountVC.swift
+//  Smack
+//
+//  Created by omrobbie on 21/01/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
