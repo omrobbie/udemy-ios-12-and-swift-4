f3184a83911ca72eb1722eef5eb2bab511333645
omrobbie <putu.roby@yahoo.com>
2020-01-21 13:52:30 +0800

    create new view controller for login

--- Smack/Smack/Controller/ChannelVC.swift
+++ Smack/Smack/Controller/ChannelVC.swift
@@ -10,7 +10,12 @@ import UIKit
 
 class ChannelVC: UIViewController {
 
+    @IBOutlet weak var loginBtn: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    @IBAction func loginBtnTapped(_ sender: Any) {
     }
 }
