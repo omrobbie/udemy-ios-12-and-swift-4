5de84b29f59ab2efcc7a8d860c978356391def55
omrobbie <putu.roby@yahoo.com>
2020-01-27 17:18:41 +0800

    setup map view layout

--- pixel-city/pixel-city/Controller/MapVC.swift
+++ pixel-city/pixel-city/Controller/MapVC.swift
@@ -7,10 +7,21 @@
 //
 
 import UIKit
+import MapKit
 
 class MapVC: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        mapView.delegate = self
     }
+
+    @IBAction func centerBtnTapped(_ sender: Any) {
+    }
+}
+
+extension MapVC: MKMapViewDelegate {
+
 }
