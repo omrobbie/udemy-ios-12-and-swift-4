dfa0f6a2b4556fe459270817ff50cc75199d9ea4
omrobbie <putu.roby@yahoo.com>
2020-01-28 17:37:33 +0800

    create new view controller for pop the image

---
+++ pixel-city/pixel-city/Controller/PopVC.swift
@@ -0,0 +1,18 @@
+//
+//  PopVC.swift
+//  pixel-city
+//
+//  Created by omrobbie on 28/01/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class PopVC: UIViewController {
+
+    @IBOutlet weak var popImageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
