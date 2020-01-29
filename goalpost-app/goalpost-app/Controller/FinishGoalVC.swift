a11288b6b56f2c3a043549fd094c446ba13df132
omrobbie <putu.roby@yahoo.com>
2020-01-29 14:00:51 +0800

    connect finish layout to controller

---
+++ goalpost-app/goalpost-app/Controller/FinishGoalVC.swift
@@ -0,0 +1,22 @@
+//
+//  FinishGoalVC.swift
+//  goalpost-app
+//
+//  Created by omrobbie on 29/01/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class FinishGoalVC: UIViewController {
+
+    @IBOutlet weak var pointsTextField: UITextField!
+    @IBOutlet weak var createGoalBtn: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func createGoalBtnWasPressed(_ sender: Any) {
+    }
+}
