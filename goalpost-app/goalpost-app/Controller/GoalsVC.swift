c0c527ba4a8a54856fbbce575ab7e50b40717917
omrobbie <putu.roby@yahoo.com>
2020-01-29 10:54:53 +0800

    setup table view cell layout

--- goalpost-app/goalpost-app/Controller/GoalsVC.swift
+++ goalpost-app/goalpost-app/Controller/GoalsVC.swift
@@ -10,7 +10,12 @@ import UIKit
 
 class GoalsVC: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    @IBAction func addGoalBtnWasPressed(_ sender: Any) {
     }
 }
