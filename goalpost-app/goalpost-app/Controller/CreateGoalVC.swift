a490f6f7deecba02653d81106f3bfb6e9c676445
omrobbie <putu.roby@yahoo.com>
2020-01-29 12:35:14 +0800

    connect new goal layout to controller

---
+++ goalpost-app/goalpost-app/Controller/CreateGoalVC.swift
@@ -0,0 +1,34 @@
+//
+//  CreateGoalVC.swift
+//  goalpost-app
+//
+//  Created by omrobbie on 29/01/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class CreateGoalVC: UIViewController {
+
+    @IBOutlet weak var goalTextView: UITextView!
+    @IBOutlet weak var shortTermBtn: UIButton!
+    @IBOutlet weak var longTermBtn: UIButton!
+    @IBOutlet weak var nextBtn: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func shortTermBtnWasPressed(_ sender: Any) {
+    }
+
+    @IBAction func longTermBtnWasPressed(_ sender: Any) {
+    }
+
+    @IBAction func nextBtnWasPressed(_ sender: Any) {
+    }
+
+    @IBAction func backBtnWasPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
