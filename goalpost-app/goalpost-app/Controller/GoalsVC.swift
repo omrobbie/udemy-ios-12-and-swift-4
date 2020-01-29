ef2498c3f6377dc5ce991e56ca132011007ae12c
omrobbie <putu.roby@yahoo.com>
2020-01-29 12:02:23 +0800

    use enum for goal type

--- goalpost-app/goalpost-app/Controller/GoalsVC.swift
+++ goalpost-app/goalpost-app/Controller/GoalsVC.swift
@@ -37,8 +37,8 @@ extension GoalsVC: UITableViewDelegate, UITableViewDataSource {
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "goalCell") as? GoalCell else {return UITableViewCell()}
-        cell.configureCell(description: "Eat salad twice a week.", type: "Short Time", progress: 2
-        )
+        cell.configureCell(description: "Eat salad twice a week", type: .shortTerm, progress: 2)
+
         return cell
     }
 }
