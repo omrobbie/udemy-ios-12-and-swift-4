0fd0d7f166335947a64cf330f2975031238767c7
omrobbie <putu.roby@yahoo.com>
2020-01-29 17:30:56 +0800

    setup completion view to the cell

--- goalpost-app/goalpost-app/View/GoalCell.swift
+++ goalpost-app/goalpost-app/View/GoalCell.swift
@@ -13,10 +13,17 @@ class GoalCell: UITableViewCell {
     @IBOutlet weak var goalDescriptionLbl: UILabel!
     @IBOutlet weak var goalTypeLbl: UILabel!
     @IBOutlet weak var goalProgressLbl: UILabel!
+    @IBOutlet weak var completionView: UIView!
 
     func configureCell(goal: Goal) {
         self.goalDescriptionLbl.text = goal.goalDescription
         self.goalTypeLbl.text = goal.goalType
         self.goalProgressLbl.text = String(describing: goal.goalProgress)
+
+        if goal.goalProgress == goal.goalCompletionValue {
+            completionView.isHidden = false
+        } else {
+            completionView.isHidden = true
+        }
     }
 }
