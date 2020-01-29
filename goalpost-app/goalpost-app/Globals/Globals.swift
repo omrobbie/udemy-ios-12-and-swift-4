126750c1b36bb07b9c52e526b027142e61a7d040
omrobbie <putu.roby@yahoo.com>
2020-01-29 15:23:17 +0800

    save data to core data

---
+++ goalpost-app/goalpost-app/Globals/Globals.swift
@@ -0,0 +1,11 @@
+//
+//  Globals.swift
+//  goalpost-app
+//
+//  Created by omrobbie on 29/01/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+let appDelegate = UIApplication.shared.delegate as? AppDelegate
