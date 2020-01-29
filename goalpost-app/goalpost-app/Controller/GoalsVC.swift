be74b457eb2eb51ad58ec3ea5eee6813a9873463
omrobbie <putu.roby@yahoo.com>
2020-01-29 11:58:35 +0800

    setup table view with dummy data

--- goalpost-app/goalpost-app/Controller/GoalsVC.swift
+++ goalpost-app/goalpost-app/Controller/GoalsVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class GoalsVC: UIViewController {
 
@@ -14,8 +15,30 @@ class GoalsVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.isHidden = false
     }
 
     @IBAction func addGoalBtnWasPressed(_ sender: Any) {
+    }
+}
+
+extension GoalsVC: UITableViewDelegate, UITableViewDataSource {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "goalCell") as? GoalCell else {return UITableViewCell()}
+        cell.configureCell(description: "Eat salad twice a week.", type: "Short Time", progress: 2
+        )
+        return cell
     }
 }
