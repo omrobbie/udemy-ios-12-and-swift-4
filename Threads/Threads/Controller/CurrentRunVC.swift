7fdde263e05c3e84605e82a928d0e681192560df
omrobbie <putu.roby@yahoo.com>
2020-01-30 15:25:43 +0800

    prepare current location view controller

--- Threads/Threads/Controller/CurrentRunVC.swift
+++ Threads/Threads/Controller/CurrentRunVC.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import MapKit
 
-class CurrentRunVC: UIViewController {
+class CurrentRunVC: LocationVC {
 
     @IBOutlet weak var swipeBgImageView: UIImageView!
     @IBOutlet weak var sliderImageView: UIImageView!
-    
+    @IBOutlet weak var durationLbl: UILabel!
+    @IBOutlet weak var paceLbl: UILabel!
+    @IBOutlet weak var distanceLbl: UILabel!
+    @IBOutlet weak var pauseBtn: UIButton!
+
+    var startLocation: CLLocation!
+    var lastLocation: CLLocation!
+    var runDistance = 0.0
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,6 +29,20 @@ class CurrentRunVC: UIViewController {
         sliderImageView.addGestureRecognizer(swipeGesture)
         sliderImageView.isUserInteractionEnabled = true
         swipeGesture.delegate = self as? UIGestureRecognizerDelegate
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        manager?.delegate = self
+        manager?.distanceFilter = 10
+        startRun()
+    }
+
+    func startRun() {
+        manager?.startUpdatingLocation()
+    }
+
+    func endRun() {
+        manager?.stopUpdatingLocation()
     }
 
     @objc func endRunSwipe(sender: UIPanGestureRecognizer) {
@@ -45,5 +68,20 @@ class CurrentRunVC: UIViewController {
                 }
             }
         }
+    }
+    
+    @IBAction func pauseBtnPressed(_ sender: Any) {
+    }
+}
+
+extension CurrentRunVC: CLLocationManagerDelegate {
+
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            checkLocationAuthStatus()
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
     }
 }
