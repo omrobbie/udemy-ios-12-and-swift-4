8a5d684b06c22495c20bd462e67f181ec7ee56dc
omrobbie <putu.roby@yahoo.com>
2020-01-30 22:09:31 +0800

    connect run log cell with controller

--- Threads/Threads/Extensions/Extensions.swift
+++ Threads/Threads/Extensions/Extensions.swift
@@ -34,3 +34,15 @@ extension Int {
         }
     }
 }
+
+extension NSDate {
+
+    func getDateString() -> String {
+        let calendar = Calendar.current
+        let month = calendar.component(.month, from: self as Date)
+        let day = calendar.component(.day, from: self as Date)
+        let year = calendar.component(.year, from: self as Date)
+
+        return "\(month)/\(day)/\(year)"
+    }
+}
