34b62418fbbcbd44f5dc5d2cd9c79db6a93dca54
omrobbie <putu.roby@yahoo.com>
2020-01-30 22:09:31 +0800

    connect run log cell with controller

---
+++ Threads/Threads/View/RunLogCell.swift
@@ -0,0 +1,24 @@
+//
+//  RunLogCell.swift
+//  Threads
+//
+//  Created by omrobbie on 30/01/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class RunLogCell: UITableViewCell {
+
+    @IBOutlet weak var runDurationLbl: UILabel!
+    @IBOutlet weak var totalDistanceLbl: UILabel!
+    @IBOutlet weak var averagePaceLbl: UILabel!
+    @IBOutlet weak var dateLbl: UILabel!
+
+    func configureCell(run: Run) {
+        runDurationLbl.text = run.duration.formatTimeDurationToString()
+        totalDistanceLbl.text = "\(run.distance.metersToMiles(places: 2)) mi"
+        averagePaceLbl.text = run.pace.formatTimeDurationToString()
+        dateLbl.text = "\(run.date.getDateString())"
+    }
+}
