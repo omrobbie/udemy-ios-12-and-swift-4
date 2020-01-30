96cf3238d98b920e43627354fb122d149dae13d1
omrobbie <putu.roby@yahoo.com>
2020-01-30 22:18:30 +0800

    draw realm data on table view

--- Threads/Threads/Controller/RunLogVC.swift
+++ Threads/Threads/Controller/RunLogVC.swift
@@ -10,7 +10,29 @@ import UIKit
 
 class RunLogVC: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+
+extension RunLogVC: UITableViewDelegate, UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Run.getAllRuns()?.count ?? 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "RunLogCell") as? RunLogCell {
+            guard let run = Run.getAllRuns()?[indexPath.row] else {return RunLogCell()}
+
+            cell.configure(run: run)
+            return cell
+        } else {
+            return RunLogCell()
+        }
     }
 }
