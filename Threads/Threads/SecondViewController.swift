12784122fa9cdf276cfbe44ae11f34dc5fb06f16
omrobbie <putu.roby@yahoo.com>
2020-01-30 10:15:17 +0800

    start threads project

---
+++ Threads/Threads/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Threads
+//
+//  Created by omrobbie on 30/01/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
