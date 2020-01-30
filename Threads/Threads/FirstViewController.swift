a1aaeacbf80a6cd6f800eea1d6549e18a62e9ddd
omrobbie <putu.roby@yahoo.com>
2020-01-30 10:51:47 +0800

    clean code

--- Threads/Threads/FirstViewController.swift
+++ Threads/Threads/FirstViewController.swift
@@ -12,9 +12,5 @@ class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
