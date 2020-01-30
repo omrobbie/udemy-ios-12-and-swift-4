f55e2340652a7943732c195cddb82de322205721
omrobbie <putu.roby@yahoo.com>
2020-01-30 14:12:59 +0800

    show current user location to the map view

--- Threads/Threads/Controller/BeginRunVC.swift
+++ Threads/Threads/Controller/BeginRunVC.swift
@@ -7,10 +7,38 @@
 //
 
 import UIKit
+import MapKit
 
-class BeginRunVC: UIViewController {
+class BeginRunVC: LocationVC {
+
+    @IBOutlet weak var mapView: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        checkLocationAuthStatus()
+        mapView.delegate = self
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        manager?.delegate = self
+        manager?.startUpdatingLocation()
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        manager?.stopUpdatingLocation()
+    }
+
+    @IBAction func locationCenterBtnPressed(_ sender: Any) {
+    }
+}
+
+extension BeginRunVC: CLLocationManagerDelegate {
+
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            checkLocationAuthStatus()
+            mapView.showsUserLocation = true
+            mapView.userTrackingMode = .follow
+        }
     }
 }
---
+++ Threads/Threads/Controller/LocationVC.swift
@@ -0,0 +1,29 @@
+//
+//  LocationVC.swift
+//  Threads
+//
+//  Created by omrobbie on 30/01/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class LocationVC: UIViewController, MKMapViewDelegate {
+
+    var manager: CLLocationManager?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        manager = CLLocationManager()
+        manager?.desiredAccuracy = kCLLocationAccuracyBest
+        manager?.activityType = .fitness
+    }
+
+    func checkLocationAuthStatus() {
+        if CLLocationManager.authorizationStatus() != .authorizedWhenInUse {
+            manager?.requestWhenInUseAuthorization()
+        }
+    }
+}
