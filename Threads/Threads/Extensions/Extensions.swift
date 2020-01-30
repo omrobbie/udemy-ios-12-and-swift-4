ac45cabd3d6a074555c0fbc905df7578d91491a4
omrobbie <putu.roby@yahoo.com>
2020-01-30 15:52:10 +0800

    convert duration time with formatting string

--- Threads/Threads/Extensions/Extensions.swift
+++ Threads/Threads/Extensions/Extensions.swift
@@ -15,3 +15,22 @@ extension Double {
         return ((self / 1609.34) * divisor).rounded() / divisor
     }
 }
+
+extension Int {
+
+    func formatTimeDurationToString() -> String {
+        let durationHours = self / 3600
+        let durationMinutes = (self % 3600) / 60
+        let durationSeconds = (self % 3600) % 60
+
+        if durationSeconds < 0 {
+            return "00:00:00"
+        } else {
+            if durationHours == 0 {
+                return String(format: "%02d:%02d", durationMinutes, durationSeconds)
+            } else {
+                return String(format: "%02d:%02d:%02d", durationHours, durationMinutes, durationSeconds)
+            }
+        }
+    }
+}
