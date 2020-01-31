d8cbb06cc69408f7c089d844cb47c04d24d1113f
omrobbie <putu.roby@yahoo.com>
2020-01-31 10:19:14 +0800

    show the last run info

--- Threads/Threads/Controller/BeginRunVC.swift
+++ Threads/Threads/Controller/BeginRunVC.swift
@@ -13,6 +13,7 @@ class BeginRunVC: LocationVC {
 
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var lastRunBgView: UIView!
+    @IBOutlet weak var lastRunStackView: UIStackView!
     @IBOutlet weak var lastRunBtn: UIButton!
     @IBOutlet weak var paceLbl: UILabel!
     @IBOutlet weak var distanceLbl: UILabel!
@@ -27,16 +28,38 @@ class BeginRunVC: LocationVC {
     override func viewWillAppear(_ animated: Bool) {
         manager?.delegate = self
         manager?.startUpdatingLocation()
+        getLastRun()
     }
 
     override func viewWillDisappear(_ animated: Bool) {
         manager?.stopUpdatingLocation()
     }
 
+    func getLastRun() {
+        guard let lastRun = Run.getAllRuns()?.first else {
+            lastRunBgView.isHidden = true
+            lastRunStackView.isHidden = true
+            lastRunBtn.isHidden = true
+            return
+        }
+
+        lastRunBgView.isHidden = false
+        lastRunStackView.isHidden = false
+        lastRunBtn.isHidden = false
+
+        paceLbl.text = lastRun.pace.formatTimeDurationToString()
+        distanceLbl.text = "\(lastRun.distance.metersToMiles(places: 2)) mi"
+        durationLbl.text = lastRun.duration.formatTimeDurationToString()
+    }
+
     @IBAction func locationCenterBtnPressed(_ sender: Any) {
     }
 
-    @IBOutlet weak var lastRunBtnPressed: NSLayoutConstraint!
+    @IBAction func lastRunBtnPressed(_ sender: Any) {
+        lastRunBgView.isHidden = true
+        lastRunStackView.isHidden = true
+        lastRunBtn.isHidden = true
+    }
 }
 
 extension BeginRunVC: CLLocationManagerDelegate {
