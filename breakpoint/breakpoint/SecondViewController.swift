3e8487d29d3e92bdd7bd40c29323ae4cec18e182
omrobbie <putu.roby@yahoo.com>
2020-02-01 09:54:05 +0800

    clean code and portrait orientation only

--- breakpoint/breakpoint/SecondViewController.swift
+++ breakpoint/breakpoint/SecondViewController.swift
@@ -12,9 +12,5 @@ class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
