0d3666a6476b3866e90aa6e56fa67b48b7ba2bec
omrobbie <putu.roby@yahoo.com>
2020-02-01 12:11:00 +0800

    create template view controller for all login and by email

---
+++ breakpoint/breakpoint/Controller/AuthVC.swift
@@ -0,0 +1,16 @@
+//
+//  AuthVC.swift
+//  breakpoint
+//
+//  Created by omrobbie on 01/02/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class AuthVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ breakpoint/breakpoint/Controller/LoginVC.swift
@@ -0,0 +1,16 @@
+//
+//  LoginVC.swift
+//  breakpoint
+//
+//  Created by omrobbie on 01/02/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
