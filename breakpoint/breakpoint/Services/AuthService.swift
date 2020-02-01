3426de1ece8156faecec921a0563ff5955856894
omrobbie <putu.roby@yahoo.com>
2020-02-01 13:52:46 +0800

    create auth service

---
+++ breakpoint/breakpoint/Services/AuthService.swift
@@ -0,0 +1,40 @@
+//
+//  AuthService.swift
+//  breakpoint
+//
+//  Created by omrobbie on 01/02/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import Foundation
+import Firebase
+import FirebaseAuth
+
+class AuthService {
+
+    static let instance = AuthService()
+
+    func registerUser(withEmail email: String, andPassword password: String, userCreationComplete: @escaping (_ status: Bool, _ error: Error?) -> ()) {
+        Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+            guard let user = user else {
+                userCreationComplete(false, error)
+                return
+            }
+
+            let userData = ["provider": user.user.providerID, "email": user.user.email]
+            DataService.instance.createDBUser(uid: user.user.uid, userData: userData)
+            userCreationComplete(true, nil)
+        }
+    }
+
+    func loginUser(withEmail email: String, andPassword password: String, loginComplete: @escaping (_ status: Bool, _ error: Error?) -> ()) {
+        Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+            if error != nil {
+                loginComplete(false, error)
+                return
+            }
+
+            loginComplete(true, nil)
+        }
+    }
+}
