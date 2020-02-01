201acc535f8def7c4cbb366c67cec4f284a5df97
omrobbie <putu.roby@yahoo.com>
2020-02-01 14:27:04 +0800

    method sign in to firebase

--- breakpoint/breakpoint/Controller/LoginVC.swift
+++ breakpoint/breakpoint/Controller/LoginVC.swift
@@ -15,12 +15,38 @@ class LoginVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        emailField.delegate = self
+        passwordField.delegate = self
     }
 
     @IBAction func signInBtnWasPressed(_ sender: Any) {
+        if emailField.text != nil && passwordField.text != nil {
+            AuthService.instance.loginUser(withEmail: emailField.text!, andPassword: passwordField.text!) { (success, error) in
+                if success {
+                    self.dismiss(animated: true, completion: nil)
+                } else {
+                    print("Error login user: \(String(describing: error?.localizedDescription))")
+                }
+
+                AuthService.instance.registerUser(withEmail: self.emailField.text!, andPassword: self.passwordField.text!) { (success, error) in
+                    if success {
+                        AuthService.instance.loginUser(withEmail: self.emailField.text!, andPassword: self.passwordField.text!) { (success, nil) in
+                            print("Successfully registered user!")
+                        }
+                    } else {
+                        print("Error register user: \(String(describing: error?.localizedDescription))")
+                    }
+                }
+            }
+        }
     }
 
     @IBAction func closeBtnWasPressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
+}
+
+extension LoginVC: UITextFieldDelegate {
+
 }
