494709b1beda8ff9a26d816d4a9c4a916154ffe7
omrobbie <putu.roby@yahoo.com>
2020-02-03 13:03:47 +0800

    connect between me layout and me controller

--- breakpoint/breakpoint/Controller/MeVC.swift
+++ breakpoint/breakpoint/Controller/MeVC.swift
@@ -10,7 +10,14 @@ import UIKit
 
 class MeVC: UIViewController {
 
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    @IBAction func signOutBtnWasPressed(_ sender: Any) {
     }
 }
