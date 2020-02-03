e4f47dd461675a3ffc7d17865535004bd62361ff
omrobbie <putu.roby@yahoo.com>
2020-02-03 17:27:12 +0800

    show data to feed cell

--- breakpoint/breakpoint/Controller/FeedVC.swift
+++ breakpoint/breakpoint/Controller/FeedVC.swift
@@ -11,11 +11,22 @@ import UIKit
 class FeedVC: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
+
+    var messageArray = [Message]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        DataService.instance.getAllFeedMessages { (returnedMessageArray) in
+            self.messageArray = returnedMessageArray
+            self.tableView.reloadData()
+        }
     }
 }
 
@@ -26,10 +37,16 @@ extension FeedVC: UITableViewDelegate, UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        return messageArray.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "feedCell") as? FeedCell else {return UITableViewCell()}
+        let image = #imageLiteral(resourceName: "defaultProfileImage")
+        let message = messageArray[indexPath.row]
+
+        cell.configureCell(profileImage: image, email: message.senderId, content: message.content)
+
+        return cell
     }
 }
