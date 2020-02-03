34ea2d6d67c69e1f123379fbe4d7adf5eb802d4b
omrobbie <putu.roby@yahoo.com>
2020-02-03 13:46:03 +0800

    upload post

--- breakpoint/breakpoint/Controller/CreatePostVC.swift
+++ breakpoint/breakpoint/Controller/CreatePostVC.swift
@@ -7,21 +7,44 @@
 //
 
 import UIKit
+import Firebase
 
 class CreatePostVC: UIViewController {
 
     @IBOutlet weak var profileImage: UIImageView!
     @IBOutlet weak var emailLbl: UILabel!
-    @IBOutlet weak var textField: UITextView!
+    @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var sendBtn: UIButton!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        textView.delegate = self
+        textView.inputAccessoryView = sendBtn
     }
 
     @IBAction func sendBtnWasPressed(_ sender: Any) {
+        if textView.text != "" && textView.text != "Say something here..." {
+            sendBtn.isEnabled = false
+            DataService.instance.uploadPost(withMessage: textView.text, forUID: (Auth.auth().currentUser?.uid)!, withGroupKey: nil) { (isComplete) in
+                if isComplete {
+                    self.sendBtn.isEnabled = true
+                    self.dismiss(animated: true, completion: nil)
+                } else {
+                    self.sendBtn.isEnabled = true
+                    print("There is an error while uploading post!")
+                }
+            }
+        }
     }
 
     @IBAction func closeBtnWasPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+extension CreatePostVC: UITextViewDelegate {
+
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textView.text = ""
     }
 }
