27fac3d92374b31cd9460c7e91a23f6f29de8991
omrobbie <putu.roby@yahoo.com>
2020-02-03 15:52:36 +0800

    create and connect feed table view cell layout

---
+++ breakpoint/breakpoint/View/FeedCell.swift
@@ -0,0 +1,22 @@
+//
+//  FeedCell.swift
+//  breakpoint
+//
+//  Created by omrobbie on 03/02/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var contentLbl: UILabel!
+
+    func configureCell(profileImage: UIImage, email: String, content: String) {
+        self.profileImage.image = profileImage
+        self.emailLbl.text = email
+        self.contentLbl.text = content
+    }
+}
