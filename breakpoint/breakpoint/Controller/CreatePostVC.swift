bc16e458aa7bb825f60e1501874f989d931bd07f
omrobbie <putu.roby@yahoo.com>
2020-02-03 13:27:03 +0800

    connect create post layout to controller

---
+++ breakpoint/breakpoint/Controller/CreatePostVC.swift
@@ -0,0 +1,27 @@
+//
+//  CreatePostVC.swift
+//  breakpoint
+//
+//  Created by omrobbie on 03/02/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class CreatePostVC: UIViewController {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var textField: UITextView!
+    @IBOutlet weak var sendBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func sendBtnWasPressed(_ sender: Any) {
+    }
+
+    @IBAction func closeBtnWasPressed(_ sender: Any) {
+    }
+}
