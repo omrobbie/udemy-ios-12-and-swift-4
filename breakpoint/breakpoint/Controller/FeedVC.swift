cbc0683419ab7c9ecb6f0e17fa95e5a829e02dd8
omrobbie <putu.roby@yahoo.com>
2020-02-03 17:21:42 +0800

    connect tableview to controller

--- breakpoint/breakpoint/Controller/FeedVC.swift
+++ breakpoint/breakpoint/Controller/FeedVC.swift
@@ -10,7 +10,26 @@ import UIKit
 
 class FeedVC: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+
+extension FeedVC: UITableViewDelegate, UITableViewDataSource {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
 }
