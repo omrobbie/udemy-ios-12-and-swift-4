26b9c370d319879e703b27104748b68436252873
omrobbie <putu.roby@yahoo.com>
2020-02-03 12:57:50 +0800

    create relation between me layout and tab bar controller

---
+++ breakpoint/breakpoint/Controller/MeVC.swift
@@ -0,0 +1,16 @@
+//
+//  MeVC.swift
+//  breakpoint
+//
+//  Created by omrobbie on 03/02/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
