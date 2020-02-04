03ceeec0e2664c979b367ba750abd1f782ae2286
omrobbie <putu.roby@yahoo.com>
2020-02-04 16:38:22 +0800

    get all messages from the group

--- breakpoint/breakpoint/Services/DataService.swift
+++ breakpoint/breakpoint/Services/DataService.swift
@@ -43,7 +43,7 @@ class DataService {
 
     func uploadPost(withMessage message: String, forUID uid: String, withGroupKey groupKey: String?, sendComplete: @escaping (_ status: Bool) -> ()) {
         if groupKey != nil {
-
+            REF_GROUPS.child(groupKey!).child("message").childByAutoId().updateChildValues(["content": message, "senderId": uid])
         } else {
             REF_FEED.childByAutoId().updateChildValues(["content": message, "senderId": uid])
             sendComplete(true)
@@ -77,6 +77,24 @@ class DataService {
             }
 
             handler(messageArray)
+        }
+    }
+
+    func getAllMessageFor(desiredGroup group: Group, handler: @escaping (_ messagesArray: [Message]) -> ()) {
+        var groupMessageArray = [Message]()
+
+        REF_GROUPS.child(group.key).child("messages").observeSingleEvent(of: .value) { (groupMessageSnapshot) in
+            guard let groupMessageSnapshot = groupMessageSnapshot.children.allObjects as? [DataSnapshot] else {return}
+
+            for groupMessage in groupMessageSnapshot {
+                let content = groupMessage.childSnapshot(forPath: "content").value as! String
+                let senderId = groupMessage.childSnapshot(forPath: "senderId").value as! String
+                let groupMessage = Message(content: content, senderId: senderId)
+
+                groupMessageArray.append(groupMessage)
+            }
+
+            handler(groupMessageArray)
         }
     }
 
