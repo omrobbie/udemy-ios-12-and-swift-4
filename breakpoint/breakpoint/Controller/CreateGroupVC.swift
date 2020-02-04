294bd7644e15c0710e6d55c5080bcb0b72376519
omrobbie <putu.roby@yahoo.com>
2020-02-04 14:02:16 +0800

    show data to  user cell

--- breakpoint/breakpoint/Controller/CreateGroupVC.swift
+++ breakpoint/breakpoint/Controller/CreateGroupVC.swift
@@ -18,10 +18,27 @@ class CreateGroupVC: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var doneBtn: UIButton!
 
+    var emailArray = [String]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        emailSearchTextField.delegate = self
+
+        emailSearchTextField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
+    }
+
+    @objc func textFieldDidChange() {
+        if emailSearchTextField.text == "" {
+            emailArray = []
+            tableView.reloadData()
+        } else {
+            DataService.instance.getEmail(forSearchQuery: emailSearchTextField.text!) { (returnedEmailArray) in
+                self.emailArray = returnedEmailArray
+                self.tableView.reloadData()
+            }
+        }
     }
 
     @IBAction func doneBtnWasPressed(_ sender: Any) {
@@ -39,15 +56,19 @@ extension CreateGroupVC: UITableViewDelegate, UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return emailArray.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "userCell") as? UserCell else {return UITableViewCell()}
         let profileImage = #imageLiteral(resourceName: "defaultProfileImage")
 
-        cell.configureCell(profileImage: profileImage, email: "user@email.com", isSelected: true)
+        cell.configureCell(profileImage: profileImage, email: emailArray[indexPath.row], isSelected: true)
 
         return cell
     }
+}
+
+extension CreateGroupVC: UITextFieldDelegate {
+
 }
