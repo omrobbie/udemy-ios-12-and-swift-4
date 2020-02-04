dbb957bad9cf9278a55dc0e44d45fd657aef86a7
omrobbie <putu.roby@yahoo.com>
2020-02-04 15:24:43 +0800

    get all group from firebase

--- breakpoint/breakpoint/Services/DataService.swift
+++ breakpoint/breakpoint/Services/DataService.swift
@@ -119,4 +119,26 @@ class DataService {
         REF_GROUPS.childByAutoId().updateChildValues(["title": title, "description": description, "members": ids])
         handler(true)
     }
+
+    func getAllGroup(handler: @escaping (_ groupArray: [Group]) -> ()) {
+        var groupArray = [Group]()
+
+        REF_GROUPS.observeSingleEvent(of: .value) { (groupSnapshot) in
+            guard let groupSnapshot = groupSnapshot.children.allObjects as? [DataSnapshot] else {return}
+
+            for group in groupSnapshot {
+                let memberArray = group.childSnapshot(forPath: "members").value as! [String]
+
+                if memberArray.contains((Auth.auth().currentUser?.uid)!) {
+                    let title = group.childSnapshot(forPath: "title").value as! String
+                    let description = group.childSnapshot(forPath: "description").value as! String
+                    let group = Group(title: title, description: description, key: group.key, memberCount: memberArray.count, members: memberArray)
+
+                    groupArray.append(group)
+                }
+            }
+
+            handler(groupArray)
+        }
+    }
 }
