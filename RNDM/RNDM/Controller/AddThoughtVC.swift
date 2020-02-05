a4d1750ec694b5660c1d7336493a069c396fc308
omrobbie <putu.roby@yahoo.com>
2020-02-05 19:23:15 +0800

    connect new post layout to add thought controller

--- RNDM/RNDM/Controller/AddThoughtVC.swift
+++ RNDM/RNDM/Controller/AddThoughtVC.swift
@@ -10,7 +10,32 @@ import UIKit
 
 class AddThoughtVC: UIViewController {
 
+    @IBOutlet weak var categorySegment: Ios12SegmentedControl!
+    @IBOutlet weak var userNameTxt: UITextField!
+    @IBOutlet weak var thoughtTxt: UITextView!
+    @IBOutlet weak var postBtn: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        postBtn.layer.cornerRadius = 4
+
+        thoughtTxt.delegate = self
+        thoughtTxt.layer.cornerRadius = 4
+        thoughtTxt.text = "my random thought..."
+        thoughtTxt.textColor = .lightGray
+    }
+
+    @IBAction func categoryChanged(_ sender: Any) {
+    }
+
+    @IBAction func postBtnTapped(_ sender: Any) {
+    }
+}
+
+extension AddThoughtVC: UITextViewDelegate {
+
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textView.text = ""
+        textView.textColor = .darkGray
     }
 }
