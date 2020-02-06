82c1c718bc8ffa1fe7ddafd0a2ee0feb17fbdc87
omrobbie <putu.roby@yahoo.com>
2020-02-06 11:36:54 +0800

    design main view controller layout

--- RNDM/RNDM/Controller/MainVC.swift
+++ RNDM/RNDM/Controller/MainVC.swift
@@ -17,7 +17,23 @@ enum ThoughtCategory: String {
 
 class MainVC: UIViewController {
 
+    @IBOutlet weak var segmentedControl: Ios12SegmentedControl!
+    @IBOutlet weak var tableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+
+extension MainVC: UITableViewDelegate, UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
 }
