213740146d33a9ed530181dfff9aa0057b79e8e2
omrobbie <putu.roby@yahoo.com>
2020-02-08 09:53:34 +0800

    create login layout and connect to controller

---
+++ RNDM/RNDM/Controller/LoginVC.swift
@@ -0,0 +1,27 @@
+//
+//  LoginVC.swift
+//  RNDM
+//
+//  Created by omrobbie on 08/02/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+    @IBOutlet weak var loginBtn: UIButton!
+    @IBOutlet weak var createUserBtn: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func loginBtnTapped(_ sender: Any) {
+    }
+
+    @IBAction func createUserBtnTapped(_ sender: Any) {
+    }
+}
