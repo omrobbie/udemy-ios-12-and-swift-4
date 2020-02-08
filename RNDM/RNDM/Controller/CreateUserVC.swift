d2edbf62bf7b06d02e439b0f070719378568c2e3
omrobbie <putu.roby@yahoo.com>
2020-02-08 10:01:56 +0800

    create new user layout and connect to controller

---
+++ RNDM/RNDM/Controller/CreateUserVC.swift
@@ -0,0 +1,28 @@
+//
+//  CreateUserVC.swift
+//  RNDM
+//
+//  Created by omrobbie on 08/02/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class CreateUserVC: UIViewController {
+
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var createBtn: UIButton!
+    @IBOutlet weak var cancelBtn: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func createBtnTapped(_ sender: Any) {
+    }
+
+    @IBAction func cancelBtnTapped(_ sender: Any) {
+    }
+}
