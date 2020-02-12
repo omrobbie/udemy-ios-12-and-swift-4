62dad8b066da08e3a526e078d3c13fcf9ea78c98
omrobbie <putu.roby@yahoo.com>
2020-02-12 17:20:27 +0800

    prepare logout button and user label

--- FirebaseSocial/FirebaseSocial/ViewController.swift
+++ FirebaseSocial/FirebaseSocial/ViewController.swift
@@ -10,7 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userInfoLbl: UILabel!
+    @IBOutlet weak var logoutBtn: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        logoutBtn.layer.cornerRadius = 10
+    }
+
+    @IBAction func logoutBtnTapped(_ sender: Any) {
     }
 }
