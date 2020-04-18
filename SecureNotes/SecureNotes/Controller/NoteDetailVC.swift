40ece9afb0d96116cf57fb9b71678c1755605d6a
omrobbie <putu.roby@yahoo.com>
2020-04-19 06:53:58 +0800

    layout note detail

---
+++ SecureNotes/SecureNotes/Controller/NoteDetailVC.swift
@@ -0,0 +1,21 @@
+//
+//  NoteDetailVC.swift
+//  SecureNotes
+//
+//  Created by omrobbie on 19/04/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class NoteDetailVC: UIViewController {
+
+    @IBOutlet weak var txtNote: UITextView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func btnLockTapped(_ sender: Any) {
+    }
+}
