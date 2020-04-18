9b55b398efbb8070725e791079b20430183547d0
omrobbie <putu.roby@yahoo.com>
2020-04-19 06:19:29 +0800

    prepare note layout

---
+++ SecureNotes/SecureNotes/Controller/NoteVC.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  SecureNotes
+//
+//  Created by omrobbie on 19/04/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class NoteVC: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+extension NoteVC: UITableViewDelegate, UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
+    }
+}
