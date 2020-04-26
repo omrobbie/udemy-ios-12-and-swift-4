8ec5c7c350fdc8c68f7dffce0e74a79b45ddec23
omrobbie <putu.roby@yahoo.com>
2020-04-26 11:55:21 +0800

    show selected ramp to the position

--- ramp-up/ramp-up/Controller/RampPlacerVC.swift
+++ ramp-up/ramp-up/Controller/RampPlacerVC.swift
@@ -13,13 +13,13 @@ import ARKit
 class RampPlacerVC: UIViewController, ARSCNViewDelegate, UIPopoverPresentationControllerDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+
+    var selectedRamp: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let scene = SCNScene(named: "art.scnassets/pipe/pipe.dae")!
         sceneView.delegate = self
         sceneView.showsStatistics = true
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -33,12 +33,29 @@ class RampPlacerVC: UIViewController, ARSCNViewDelegate, UIPopoverPresentationCo
         sceneView.session.pause()
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else {return}
+        let result = sceneView.hitTest(touch.location(in: sceneView), types: [.featurePoint])
+        guard let hitFeature = result.last else {return}
+        let hitTransform = SCNMatrix4(hitFeature.worldTransform)
+        let hitPosition = SCNVector3(hitTransform.m41, hitTransform.m42, hitTransform.m43)
+        placeRamp(position: hitPosition)
+    }
+
     func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
         .none
     }
 
     func onRampSelected(_ rampName: String) {
-        print(rampName)
+        selectedRamp = rampName
+    }
+
+    fileprivate func placeRamp(position: SCNVector3) {
+        guard let rampName = selectedRamp else {return}
+        let scene = SCNScene(named: "art.scnassets/\(rampName).dae")!
+        let node = scene.rootNode.childNode(withName: rampName, recursively: true)!
+        node.position = position
+        sceneView.scene.rootNode.addChildNode(node)
     }
 
     @IBAction func btnAddTapped(_ sender: UIButton) {
