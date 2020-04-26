d2725ee5697a4b1e517f329af7b59e2bb3410270
omrobbie <putu.roby@yahoo.com>
2020-04-26 10:21:51 +0800

    setup camera for the scene

--- ramp-up/ramp-up/Controller/RampPickerVC.swift
+++ ramp-up/ramp-up/Controller/RampPickerVC.swift
@@ -33,6 +33,10 @@ class RampPickerVC: UIViewController {
         let scene = SCNScene(named: "art.scnassets/ramps.scn")!
         sceneView.scene = scene
 
+        let camera = SCNCamera()
+        camera.usesOrthographicProjection = true
+        scene.rootNode.camera = camera
+
         let obj = SCNScene(named: "art.scnassets/pipe/pipe.dae")
         let node = obj?.rootNode.childNode(withName: "pipe", recursively: true)!
         scene.rootNode.addChildNode(node!)
