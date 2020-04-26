adb367b93fa0fa7488c1f3624382539122aab03e
omrobbie <putu.roby@yahoo.com>
2020-04-26 09:55:16 +0800

    create ramp picker controller

---
+++ ramp-up/ramp-up/Controller/RampPickerVC.swift
@@ -0,0 +1,34 @@
+//
+//  RampPickerVC.swift
+//  ramp-up
+//
+//  Created by omrobbie on 26/04/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+
+class RampPickerVC: UIViewController {
+
+    var sceneView: SCNView!
+    var size: CGSize!
+
+    init(size: CGSize) {
+        super.init(nibName: nil, bundle: nil)
+        self.size = size
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.frame = CGRect(origin: CGPoint.zero, size: size)
+        sceneView = SCNView(frame: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+        view.insertSubview(sceneView, at: 0)
+        preferredContentSize = size
+    }
+}
