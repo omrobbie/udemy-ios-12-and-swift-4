3faeb1b4da1d2a4e4f084e945505c4dee8cfeab6
omrobbie <putu.roby@yahoo.com>
2020-04-26 08:28:22 +0800

    clean up

---
+++ ramp-up/ramp-up/RampPlacerVC.swift
@@ -0,0 +1,35 @@
+//
+//  RampPlacerVC.swift
+//  ramp-up
+//
+//  Created by omrobbie on 26/04/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class RampPlacerVC: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        sceneView.delegate = self
+        sceneView.showsStatistics = true
+        sceneView.scene = scene
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let configuration = ARWorldTrackingConfiguration()
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        sceneView.session.pause()
+    }
+}
