efc1a47404f49e4f57e728b8780ae2b4e08f6468
omrobbie <putu.roby@yahoo.com>
2020-05-19 12:13:09 +0800

    detect camera input and output

--- vision-app-dev/vision-app-dev/Controller/CameraVC.swift
+++ vision-app-dev/vision-app-dev/Controller/CameraVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraVC: UIViewController {
 
@@ -16,7 +17,38 @@ class CameraVC: UIViewController {
     @IBOutlet weak var lblConfidence: UILabel!
     @IBOutlet weak var viewCamera: RoundedShadowView!
 
+    var captureSession: AVCaptureSession!
+    var cameraOutput: AVCapturePhotoOutput!
+    var previewLayer: AVCaptureVideoPreviewLayer!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        previewLayer.frame = viewCamera.bounds
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        captureSession = AVCaptureSession()
+        captureSession.sessionPreset = AVCaptureSession.Preset.hd1920x1080
+
+        let backCamera = AVCaptureDevice.default(for: AVMediaType.video)
+
+        do {
+            let input = try AVCaptureDeviceInput(device: backCamera!)
+            if captureSession.canAddInput(input) {
+                captureSession.addInput(input)
+            }
+
+            cameraOutput = AVCapturePhotoOutput()
+            if captureSession.canAddOutput(cameraOutput) {
+                captureSession.addOutput(cameraOutput)
+            }
+        } catch {
+            debugPrint("Error: \(error.localizedDescription)")
+        }
     }
 }
