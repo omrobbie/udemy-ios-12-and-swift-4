5fb4546694913f8ad4ae6a081ca4b4d1985da153
omrobbie <putu.roby@yahoo.com>
2020-05-19 12:51:04 +0800

    capture the image

--- vision-app-dev/vision-app-dev/Controller/CameraVC.swift
+++ vision-app-dev/vision-app-dev/Controller/CameraVC.swift
@@ -21,6 +21,8 @@ class CameraVC: UIViewController {
     var cameraOutput: AVCapturePhotoOutput!
     var previewLayer: AVCaptureVideoPreviewLayer!
 
+    var photoData: Data?
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -34,6 +36,10 @@ class CameraVC: UIViewController {
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+
+        let tap = UITapGestureRecognizer(target: self, action: #selector(didTapCameraView))
+        tap.numberOfTouchesRequired = 1
+
         captureSession = AVCaptureSession()
         captureSession.sessionPreset = AVCaptureSession.Preset.hd1920x1080
 
@@ -57,10 +63,36 @@ class CameraVC: UIViewController {
                 previewLayer.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
 
                 viewCamera.layer.addSublayer(previewLayer)
+                viewCamera.addGestureRecognizer(tap)
                 captureSession.startRunning()
             }
         } catch {
             debugPrint("Error: \(error.localizedDescription)")
+        }
+    }
+
+    @objc func didTapCameraView() {
+        let settings = AVCapturePhotoSettings()
+        let previewPixelType = settings.availablePreviewPhotoPixelFormatTypes.first!
+        let previewFormat = [
+            kCVPixelBufferPixelFormatTypeKey as String: previewPixelType,
+            kCVPixelBufferWidthKey as String: 160,
+            kCVPixelBufferHeightKey as String: 160,
+        ]
+
+        settings.previewPhotoFormat = previewFormat
+        cameraOutput.capturePhoto(with: settings, delegate: self)
+    }
+}
+
+extension CameraVC: AVCapturePhotoCaptureDelegate {
+
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        if let error = error {
+            debugPrint("Error: \(error.localizedDescription)")
+        } else {
+            photoData = photo.fileDataRepresentation()
+            imgCaptured.image = UIImage(data: photoData!)
         }
     }
 }
