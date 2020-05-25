fa2c89e8e4c3d70bbda04cb413ab17fbfb9493d2
omrobbie <putu.roby@yahoo.com>
2020-05-25 21:17:03 +0800

    setup layout

--- FaceFinder/FaceFinder/ViewController.swift
+++ FaceFinder/FaceFinder/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    @IBOutlet weak var lblMessage: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
