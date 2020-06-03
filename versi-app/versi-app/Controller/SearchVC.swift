4de8fc3554f6fff38ce715fdcb7feba164f0bfdb
omrobbie <putu.roby@yahoo.com>
2020-06-03 15:25:44 +0800

    add search view controller

--- versi-app/versi-app/Controller/SearchVC.swift
+++ versi-app/versi-app/Controller/SearchVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SearchVC.swift
 //  versi-app
 //
 //  Created by omrobbie on 03/06/20.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SearchVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ versi-app/versi-app/Controller/TrendingFeedVC.swift
@@ -0,0 +1,16 @@
+//
+//  TrendingFeedVC.swift
+//  versi-app
+//
+//  Created by omrobbie on 03/06/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class TrendingFeedVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
