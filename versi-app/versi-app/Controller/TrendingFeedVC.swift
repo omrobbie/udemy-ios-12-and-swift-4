3aab5ce13fe1ffa636bda809cd2641e26d409f0c
omrobbie <putu.roby@yahoo.com>
2020-06-03 16:03:16 +0800

    show trending table view with empty data

--- versi-app/versi-app/Controller/TrendingFeedVC.swift
+++ versi-app/versi-app/Controller/TrendingFeedVC.swift
@@ -14,5 +14,19 @@ class TrendingFeedVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+
+extension TrendingFeedVC: UITableViewDelegate, UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else {return UITableViewCell()}
+        return cell
     }
 }
