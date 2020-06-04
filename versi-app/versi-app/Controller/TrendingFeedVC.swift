426c323783c491e2d8a02094b900cb74a54cb3eb
omrobbie <putu.roby@yahoo.com>
2020-06-04 11:50:45 +0800

    add refresh control

--- versi-app/versi-app/Controller/TrendingFeedVC.swift
+++ versi-app/versi-app/Controller/TrendingFeedVC.swift
@@ -17,17 +17,31 @@ class TrendingFeedVC: UIViewController {
     let dataSource = PublishSubject<[Repo]>()
     let disposeBag = DisposeBag()
 
+    let refreshControl = UIRefreshControl()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.refreshControl = refreshControl
+        refreshControl.tintColor = #colorLiteral(red: 0.2784313725, green: 0.5490196078, blue: 0.9019607843, alpha: 1)
+        refreshControl.attributedTitle = NSAttributedString(
+            string: "Fetching Hot GitHub Repos 🔥",
+            attributes: [
+                NSAttributedString.Key.foregroundColor: #colorLiteral(red: 0.2784313725, green: 0.5490196078, blue: 0.9019607843, alpha: 1),
+                NSAttributedString.Key.font: UIFont(name: "AvenirNext-DemiBold", size: 16.0)!
+            ]
+        )
+        refreshControl.addTarget(self, action: #selector(fetchData), for: .valueChanged)
+
         fetchData()
         dataSource.bind(to: tableView.rx.items(cellIdentifier: "cell")) { (row, repo: Repo, cell: TrendingRepoCell) in
             cell.parseData(item: repo)
         }.disposed(by: disposeBag)
     }
 
-    func fetchData() {
+    @objc func fetchData() {
         DownloadService.instance.downloadTrendingRepos { (trendingRepoArray) in
             self.dataSource.onNext(trendingRepoArray)
+            self.refreshControl.endRefreshing()
         }
     }
 }
