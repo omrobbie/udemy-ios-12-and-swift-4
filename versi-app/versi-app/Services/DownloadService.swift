557c415a2587141a679531d290d9217b8f4c7673
omrobbie <putu.roby@yahoo.com>
2020-06-04 09:43:53 +0800

    unwrap  all properties data

--- versi-app/versi-app/Services/DownloadService.swift
+++ versi-app/versi-app/Services/DownloadService.swift
@@ -17,11 +17,34 @@ class DownloadService {
         var trendingRepoArray = [Dictionary<String, Any>]()
         Alamofire.request(trendingRepoUrl).responseJSON { (response) in
             guard let json = response.result.value as? Dictionary<String, Any> else {return}
-            guard let repoDictionary = json["items"] as? [Dictionary<String, Any>] else {return}
+            guard let repoDictionaryArray = json["items"] as? [Dictionary<String, Any>] else {return}
 
-            for repoDict in repoDictionary {
+            for repoDict in repoDictionaryArray {
                 if trendingRepoArray.count < 10 {
-                    trendingRepoArray.append(repoDict)
+                    guard let avatarUrl = repoDict["avatar_url"] as? String,
+                        let name = repoDict["name"] as? String,
+                        let description = repoDict["description"] as? String,
+                        let numberOfForks = repoDict["forks_count"] as? Int,
+                        let language = repoDict["language"] as? String,
+                        let contributorsUrl = repoDict["contributors_url"] as? String,
+                        let ownerDict = repoDict["owner"] as? Dictionary<String, Any>,
+                        let repoUrl = repoDict["html_url"] as? String
+                        else {
+                            break
+                    }
+
+                    let repoDictionary: Dictionary<String, Any> = [
+                        "avatarUrl": avatarUrl,
+                        "name": name,
+                        "description": description,
+                        "numberOfForks": numberOfForks,
+                        "language": language,
+                        "contributorsUrl": contributorsUrl,
+                        "ownerDict": ownerDict,
+                        "repoUrl": repoUrl,
+                    ]
+
+                    trendingRepoArray.append(repoDictionary)
                 } else {
                     break
                 }
@@ -45,12 +68,12 @@ class DownloadService {
     }
 
     func downloadTrendingRepo(fromDictionary dict: Dictionary<String, Any>) -> Repo {
-//        let avatarUrl = dict["avatar_url"] as! String
+        //        let avatarUrl = dict["avatar_url"] as! String
         let name = dict["name"] as! String
         let description = dict["description"] as! String
         let numberOfForks = dict["forks_count"] as! Int
-//        let language = dict["language"] as! String
-//        let numberOfContributors = dict[""]
+        //        let language = dict["language"] as! String
+        //        let numberOfContributors = dict[""]
         let repoUrl = dict["html_url"] as! String
 
         let repo = Repo(image: UIImage(named: "searchIconLarge")!, name: name, description: description, numberOfForks: numberOfForks, language: "language", numberOfContributors: 123, repoUrl: repoUrl)
